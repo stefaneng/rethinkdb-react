0bb6a20540a6e954356518a22f87efc226e824a5
Stefan Eng <stefaneng13@gmail.com>
2015-10-17 21:27:22 -0700

    On disconnect message

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+  });
 });
 
 http.listen(3000, function(){
