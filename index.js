8fab42c7e9ff8bae26cfbbe04746a2a2afb7e343
Stefan Eng <stefaneng13@gmail.com>
2015-10-17 22:36:20 -0700

    Webpack built through express server
    Updates and server using webpackMiddleware

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var webpackMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var config = require('./webpack.config.js');
+
+var production = process.env.NODE_ENV === 'production';
 
 app.use(express.static(__dirname + '/public'));
+
+if (!production) {
+  var compiler = webpack(config);
+
+  app.use(webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  }));
+
+//  app.use(webpackHotMiddleware(compiler));
+}
 
 io.on('connection', function(socket) {
   console.log('a user connected');
