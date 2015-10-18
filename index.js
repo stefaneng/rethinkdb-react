29a5732131951cecc9f311e0f7ea0451eba420cc
Stefan Eng <stefaneng13@gmail.com>
2015-10-17 21:57:23 -0700

    Webpack with socket.io and express

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
   console.log('a user connected');
